76f24338d88f89cae37f6890b51e1be7c45236e0
Jake-Zaruba <zarubajake@gmail.com>
2022-05-25 18:00:49 -0500

    Section 12

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,7 @@
 const fs = require(`fs`);
 const http = require(`http`);
+const path = require("path");
+const url = require(`url`);
 
 ////////////////////////////////
 /////FILES/////
@@ -30,9 +32,23 @@ const http = require(`http`);
 ///////////////////////////////
 /////SERVER/////
 const server = http.createServer((req, res) => {
-  res.end(`Hello from the server!`);
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === `/` || pathName === `/overview`) {
+    res.end(`This is the overview page`);
+  } else if (pathName === `/product`) {
+    res.end(`This is the product`);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end(`<h1>Page not found</h1>`);
+  }
 });
 
 server.listen(8000, `127.0.0.1`, () => {
-  console.log(`Listening to request on port 8000`);
+  console.log(`Listening to request on port 3000`);
 });
